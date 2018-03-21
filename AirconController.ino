1162e6db80bcefcf197f7b13f8893daa28ab2562
Mateusz Giedziński <mateusz.giedzinski@intive.com>
2018-03-21 15:10:13 +0100

    DNS i HTTP serwer

--- AirconController.ino
+++ AirconController.ino
@@ -1,49 +1,44 @@
-#ifndef UNIT_TEST
-#include <Arduino.h>
-#endif
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
+#include "DNSServer.h"   
 
-ESP8266WebServer server(80);
-const char* ssid = "espAP";
+const char* ssid = "esp8266";
 const char* password = "qwerty123";
+const byte        DNS_PORT = 53;          // Capture DNS requests on port 53
+IPAddress         apIP(10, 10, 10, 10);    // Private network for server
+DNSServer         dnsServer;              // Create the DNS object 
+// TCP server at port 80 will respond to HTTP requests
+ESP8266WebServer server(80);
+
+void setup(void)
+{  
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  // configure access point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(ssid,password); // WiFi name
+  dnsServer.start(DNS_PORT, "www.AIRCON.com", apIP);
+  // Wait for connection
+  server.onNotFound(notFound);
+  server.on("/", handleRoot);
+  server.begin();
+
+  Serial.println("TCP server started");
+}
+
+  void loop() {
+    dnsServer.processNextRequest();
+    server.handleClient();
+  }
+
+void notFound() {
+  server.send(200, "text/html", "<h1>Wrong request</h1>");
+}
 void handleRoot() {
 	server.send(200, "text/html", "<h1>You are connected</h1>");
 }
 
-  void setup()
-  {
-    Serial.begin(115200);
-    Serial.println();
-  
-    Serial.print("Setting soft-AP ... ");
-    
-    IPAddress ip(192, 168, 43, 1);
-    IPAddress gateway(192, 168, 43, 1);
-    IPAddress subnet(255, 255, 255, 0);
-    WiFi.softAPConfig(ip, gateway, subnet);
-    boolean result = WiFi.softAP(ssid, password);
-    if(result == true)
-    {
-      Serial.println("Ready");
-      IPAddress myIP = WiFi.softAPIP();
-      Serial.print("AP IP address: ");
-      Serial.println(myIP);
-      server.on("/", handleRoot);
-      server.begin();
-      Serial.println("HTTP server started");
-    }
-    else
-    {
-      Serial.println("Failed!");
-    }
-
-  }
-  
-  void loop()
-  {
-    server.handleClient();
-  }
 
 
