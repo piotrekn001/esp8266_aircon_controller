f6e0dc4eae9804da30e48c26b11019018ac3dd91
Krzysztof Myhan <krzycho@wapp.pl>
2018-04-04 14:13:30 +0200

    Gree airconditioner functions.

--- AirconController.ino
+++ AirconController.ino
@@ -1,6 +1,60 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "DNSServer.h"   
+#include "DNSServer.h"
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+IRsend irsend(4);  // An IR LED is controlled by GPIO pin 4 (D2)
+
+typedef struct
+{
+        unsigned char mode : 3;
+        unsigned char onOff : 1;
+        unsigned char fan : 2;
+        unsigned char pos : 1;
+        unsigned char sleep : 1; // 8
+        unsigned char temp : 4; // 12
+        unsigned char unknown1 : 8; // 20
+        unsigned char turbo : 1;
+        unsigned char light : 1;
+        unsigned char pine : 1;
+        unsigned char xfan : 1; // 24
+        unsigned char home : 1; // 25
+        unsigned char unknown2 : 8;
+        unsigned char unknown3 : 2; // 35
+        unsigned char posSide : 4; // 39
+        unsigned char posBottom : 4; // 43
+        unsigned char tempout : 1; // 44
+        unsigned char tempin : 1; // 45
+        unsigned char ifeel : 1; // 46
+        unsigned char unknown4 : 8; // 52
+        unsigned char unknown5 : 8; // 60
+        unsigned char unknown6 : 2; // 62
+} __attribute__((packed)) packet;
+
+uint8_t calcCrc(packet *p) {
+  unsigned long long crcMask;
+  packet pMask;
+  memset(&pMask, 0, 8);
+  pMask.mode = 0x7;
+  pMask.onOff = 0x1;
+  pMask.temp = 0xf;
+  pMask.posBottom = 0xf;
+  pMask.home = 0x1;
+  memcpy(&crcMask, &pMask, 8);
+
+  unsigned long long ppp;
+  memcpy(&ppp, p, 8);
+  ppp = ppp & crcMask;
+
+  unsigned long long calcCrc = (((ppp >> 0)  & 0x0f) +
+                                ((ppp >> 8)  & 0x0f) +
+                                ((ppp >> 24) & 0x0f) +
+                                ((ppp >> 39) & 0x0f) +
+                                       0x0a) & 0x0f;
+
+  return (uint8_t) calcCrc;
+}
 
 const char* ssid = "esp8266";
 const char* password = "qwerty123";
@@ -9,6 +63,65 @@ IPAddress         apIP(10, 10, 10, 10);    // Private network for server
 DNSServer         dnsServer;              // Create the DNS object 
 // TCP server at port 80 will respond to HTTP requests
 ESP8266WebServer server(80);
+
+void sendUpdate(uint8_t mode, uint8_t temp) {
+  packet p;
+  memset(&p, 0, 8);
+  p.mode = mode;
+  p.onOff = 1;
+  p.temp = temp - 16;
+
+  p.light = 1;
+  p.pine = 1;
+  p.unknown2 = 40;
+  p.unknown3 = 1;
+  uint8_t crc = calcCrc(&p);
+  unsigned long long pp;
+  
+  memcpy(&pp, &p, 8);
+  
+  uint16_t rawData[139];
+  int i = 0;
+  rawData[i++] = 9040;
+  rawData[i++] = 4440;
+
+  for (int j = 0; j <= 34; j++) {
+    rawData[i++] = 696;
+    if ((pp >> j) & 0x01) {
+      rawData[i++] = 1600;
+    }
+    else {
+      rawData[i++] = 510;
+    }
+  }
+
+  rawData[i++] = 696;
+  rawData[i++] = 20000;
+
+  for (int j = 35; j <= 62; j++) {
+    rawData[i++] = 696;
+    if ((pp >> j) & 0x01) {
+      rawData[i++] = 1600;
+    }
+    else {
+      rawData[i++] = 510;
+    }
+  }
+
+  for (int j = 0; j <= 3; j++) {
+    rawData[i++] = 696;
+    if ((crc >> j) & 0x01) {
+      rawData[i++] = 1600;
+    }
+    else {
+      rawData[i++] = 510;
+    }
+  }
+
+  rawData[i++] = 696;
+
+  irsend.sendRaw(rawData, 139, 38);  // Send a raw data capture at 38kHz.
+}
 
 void setup(void)
 {  
